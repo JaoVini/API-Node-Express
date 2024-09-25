0d63ad264aa23309c2db3ae2ccb3a2f17017e9b4
João Vinícius <148662647+JaoVini@users.noreply.github.com>
2024-09-25 20:48:02 -0300

    Criptografando senha e gerando autenticação
    Criptografando a senha do usuário com bcrypt e gerando autenticação de acesso com jsonwebtoken

--- routes/public.js
+++ routes/public.js
@@ -1,21 +1,61 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 const prisma = new PrismaClient()
 
+const JWT_SECRET = process.env.JWT_SECRET
+
 //Cadastro
 router.post('/cadastro', async (req, res) => {
+    try {
     const user = req.body
 
-    await prisma.user.create({
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(user.password, salt) //criptografando a senha
+
+    const userDB = await prisma.user.create({
         data: {
             email: user.email,
             user: user.name,
-            password
-        }
+            password: hashPassword,
+        },
     })
-    res.status(201).json(user)
+    res.status(201).json(userDB)
+    } catch (err) {
+        res.status(500).json({message: "Erro no Servidor, tente novamente."})
+    }
 })
+
+//login
+router.post('/login', async (req, res) =>{
+    try{
+        const userInfor = req.body
+
+        //busca o usuário no banco de dados
+        const user = await prisma.user.findUnique({ where: {email: userInfor.email}})
+
+        //verifica se o usuário existe
+        if(!user){
+            return res.status(404).json({message: "Usuário não encontrado."})
+        }
+
+        //compara a senha do banco com a digitada
+        const isMatch = await bcrypt.compare(userInfor.password, user.password)
+        if (!isMatch){
+            return res.status(400).json({message: "Senhha inválida."})
+        }
+
+        //gerar o Token JWT
+        const token = jwt.sign({id: user.id}, JWT_SECRET, { expiresIn: '1m'})
+
+        res.status(200).json(token)
+    } catch (err) {
+        res.status(500).json({message: "Erro no Servidor, tente novamente."})
+    }
+})
+
 
 export default router
