16d23d2595c484de43bad1d05d84627eb44bdd50
João Vinícius <148662647+JaoVini@users.noreply.github.com>
2024-09-26 17:45:40 -0300

    utilizando a função compare do bcrypt
     comparando o valor da senha não criptografada que foi digitada com a senha hash armazenada no banco de dados

--- routes/public.js
+++ routes/public.js
@@ -1,6 +1,6 @@
-import express from 'express'
-import { PrismaClient } from '@prisma/client'
+import express from "express";
 import bcrypt from 'bcrypt'
+import { PrismaClient } from '@prisma/client'
 import jwt from 'jsonwebtoken'
 
 const router = express.Router()
@@ -8,54 +8,59 @@ const prisma = new PrismaClient()
 
 const JWT_SECRET = process.env.JWT_SECRET
 
-//Cadastro
+
+//cadastro
 router.post('/cadastro', async (req, res) => {
-    try {
-    const user = req.body
+    try{
+        const user = req.body
 
-    const salt = await bcrypt.genSalt(10)
-    const hashPassword = await bcrypt.hash(user.password, salt) //criptografando a senha
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(user.password, salt)
 
-    const userDB = await prisma.user.create({
-        data: {
-            email: user.email,
-            user: user.name,
-            password: hashPassword,
-        },
-    })
-    res.status(201).json(userDB)
+        const userDB = await prisma.user.create({
+            data: {
+                email: user.email,
+                name: user.name,
+                password: hashPassword,
+            },
+        })
+        res.status(201).json(userDB)
     } catch (err) {
-        res.status(500).json({message: "Erro no Servidor, tente novamente."})
+        res.status(500).json( {message: "1 - Erro no servidor, tente novamente"})
     }
 })
 
 //login
-router.post('/login', async (req, res) =>{
+router.post('/login', async (req, res) => {
     try{
-        const userInfor = req.body
+        const userInfo = req.body
 
-        //busca o usuário no banco de dados
-        const user = await prisma.user.findUnique({ where: {email: userInfor.email}})
+        // busca o usuário no banco de dados
+        const user = await prisma.user.findUnique({
+            where: {email: userInfo.email},
+        })
 
-        //verifica se o usuário existe
+
+        // verifica se o usuário existe no banco de dados
         if(!user){
-            return res.status(404).json({message: "Usuário não encontrado."})
+            return res.status(404).json( {message: "Usuário não encontrado 🤨"})
         }
 
-        //compara a senha do banco com a digitada
-        const isMatch = await bcrypt.compare(userInfor.password, user.password)
-        if (!isMatch){
-            return res.status(400).json({message: "Senhha inválida."})
+        // compara a senha do banco com a que a do usuário digitou
+        const isMatch = await bcrypt.compare(userInfo.password, user.password)
+
+        if(!isMatch){
+            return res.status(400).json({ message: "Senha inválida!"})
         }
 
-        //gerar o Token JWT
-        const token = jwt.sign({id: user.id}, JWT_SECRET, { expiresIn: '1m'})
+        // gerar o token JWT
+        const token = jwt.sign({ id: user.id}, JWT_SECRET, { expiresIn: '1m'})
 
         res.status(200).json(token)
-    } catch (err) {
-        res.status(500).json({message: "Erro no Servidor, tente novamente."})
+    } catch(err) {
+        res.status(500).json({message: "2- Erro no Servidor, tente novamente"})
     }
-})
 
+})
 
 export default router
